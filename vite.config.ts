c81e76e28c9f2e8272c92c2ff931587acf0a0da9
sethverrill <verrill.seth@gmail.com>
2025-02-23 08:41:11 -0700

    Fix typos, reinstall postcss with tailwind

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,20 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
-import tailwindcss from 'tailwindcss';
+import tailwindcss from '@tailwindcss/vite';
 import autoprefixer from 'autoprefixer';
+import tailwindcssPostCSS from '@tailwindcss/postcss';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    tailwindcss(),
+  ],
   css: {
     postcss: {
-      plugins: [tailwindcss, autoprefixer],
+      plugins: [
+        tailwindcssPostCSS,
+        autoprefixer,
+      ],
     },
   },
 });
